87c1f1ac42f17bbb12ae6c31b0cb51a666c689fd
LuisArtur31 <138621102+LuisArtur31@users.noreply.github.com>
2023-10-15 20:57:39 -0300

    Add files via upload

---
+++ Rotas.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Cadastrar } from "./src/Componentes/CadastrarLogin";
+import { ListarProdutos } from "./src/Componentes/ListarProdutos";
+import { Login } from "./src/Componentes/Login";
+import { ManterProdutos } from "./src/Componentes/ManterProdutos";
+
+export function Rotas() {
+    const Stack = createNativeStackNavigator()
+
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Login">
+                <Stack.Screen name = 'Login' component={Login}/>
+                <Stack.Screen name = 'Cadastrar' component={Cadastrar}/>
+                <Stack.Screen name = 'Manter' component={ManterProdutos} options={{title: 'Cadastrar/Atualizar Produtos'}}/>
+                <Stack.Screen name = 'Listar' component={ListarProdutos} options={{title: 'Cadastro de Produtos'}}/>  
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
